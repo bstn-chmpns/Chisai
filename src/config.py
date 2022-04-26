d8e3e33f4c0f7e744bd4e5843931996c0e4810c4
bstn-chmpns <59841900+bstn-chmpns@users.noreply.github.com>
2022-04-26 10:41:01 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-content_folder = ['art', 'blog', 'naming-demo']
+content_folder = ['art', 'blog', 'naming-demo', 'journal']
 relative_build_url = "/"
 absolute_build_url = "https://bstn-chmps.github.io/Chisai/"
 build_folder = "build/"
